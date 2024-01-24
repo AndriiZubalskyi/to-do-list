2d32acba47fd8daa0189ad22ad691866cf1be489
AndriiZubalskyi <and.zubal@gmail.com>
2024-01-24 18:52:12 +0200

    Modified controller to retrieve tasks

--- to-do-list/Controllers/TaskController.cs
+++ to-do-list/Controllers/TaskController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using to_do_list.Data;
 
 namespace to_do_list.Controllers
 {
     public class TaskController : Controller
     {
+        private readonly ApplicationDBContext _db;
+
+        public TaskController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            var tasksList = _db.Tasks.ToList();
             return View();
         }
     }
