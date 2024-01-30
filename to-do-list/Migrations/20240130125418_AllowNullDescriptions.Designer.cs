6b736c8784540d706830f1a8b879b470b6529703
AndriiZubalskyi <and.zubal@gmail.com>
2024-01-30 15:07:22 +0200

    Allowed Description to be null. Added maximum length of Name and Description

--- to-do-list/Migrations/20240130125418_AllowNullDescriptions.Designer.cs
+++ to-do-list/Migrations/20240130125418_AllowNullDescriptions.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using to_do_list.Data;
 
@@ -11,9 +12,10 @@ using to_do_list.Data;
 namespace to_do_list.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240130125418_AllowNullDescriptions")]
+    partial class AllowNullDescriptions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- to-do-list/Models/Task.cs
+++ to-do-list/Models/Task.cs
@@ -8,8 +8,10 @@ namespace to_do_list.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
-        public string Description { get; set; }
+        [MaxLength(150)]
+        public string? Description { get; set; }
         public Priority Priority { get; set; }
         [AllowNull]
         public DateTime? Deadline { get; set; }
